98f021870918da0dbaff4340453c0060d4ff0660
Eduardo <pertuzvillegaseduardoisaac@gmail.com>
2025-12-11 12:34:58 -0500

    initial structure

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import Image from "next/image";
+import vetImg from "@/public/image.png";
+import { Clock, Siren, Bell, Calendar, Heart, Search, AlertTriangle, User, MapPin } from "lucide-react";
+
+
+export default function Home() {
+  return (
+    <>
+      hola mundo
+    </>
+  );
+}
